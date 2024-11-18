3c69dcc1419c9a1df374bd435c2c143f8036f834
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-11-18 08:41:31 +0330

    create new task modal

--- src/componentes/Modal.tsx
+++ src/componentes/Modal.tsx
@@ -1,7 +1,8 @@
-import { Box, Modal, Paper } from "@mui/material";
+import { Box, Modal, Paper, TextField, Typography } from "@mui/material";
 import MyButton from "./common/Button";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
+import Input from "./common/Input";
 
 const MyModal = () => {
   const [open, setOpen] = useState(false);
@@ -18,8 +19,61 @@ const MyModal = () => {
         aria-describedby="modal-modal-description"
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
-        <Box sx={{ width: "40%", padding: 2 }}>
-          <Paper elevation={3}>hi</Paper>
+        <Box sx={{ width: { xs: "90%", md: "60%", lg: "40%" }, padding: 2 }}>
+          <Paper
+            elevation={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "30px",
+              gap: "20px",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h3" color="#133E87">
+              {" "}
+              New Task
+            </Typography>
+            <Input onChangeHandler={() => {}} placeholder="Title" type="text" />
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              width="100%"
+              gap="10px"
+              sx={{ flexDirection: { xs: "column", md: "row" } }}
+            >
+              <Input
+                onChangeHandler={() => {}}
+                placeholder="Priority"
+                type="text"
+              />
+              <Input
+                onChangeHandler={() => {}}
+                placeholder="Status"
+                type="text"
+              />
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              width="100%"
+              gap="10px"
+              sx={{ flexDirection: { xs: "column", md: "row" } }}
+            >
+              <Input
+                onChangeHandler={() => {}}
+                placeholder="Date"
+                type="text"
+              />
+              <Input
+                onChangeHandler={() => {}}
+                placeholder="Estimate"
+                type="text"
+              />
+            </Box>
+            <MyButton onClickHandler={() => {}} text="Add Task" width="100%" />
+          </Paper>
         </Box>
       </Modal>
     </>
