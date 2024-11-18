e408c29aabb1667e7ce42efb7e26c2c9ded8a640
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-11-18 12:42:55 +0330

    create searchbar cm

---
+++ src/componentes/pages/search-bar/SearchBar.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+const SearchBar = () => {
+  return (
+    <Box width="80%" padding={2} border="1px solid red" bgcolor="#133E87"></Box>
+  );
+};
+
+export default SearchBar;
